3c0006da9c791905888e292e4eae4685577dac7b
AshitoshAmbilwade <ashitoshambilwade1289@gmail.com>
2024-07-28 13:10:31 +0530

    add the portfolio code

---
+++ main.js
@@ -0,0 +1,10 @@
+// Ensure this is placed in main.js file
+document.addEventListener('DOMContentLoaded', function() {
+    var typed = new Typed(".text", {
+        strings: ["Full Stack Developer", "MERN Stack Developer", "Frontend Developer", "YouTuber"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
